c0b129e99e231daf471de26bad1aef023afacf0f
Inpyo-Hong <64252911+Inpyo-Hong@users.noreply.github.com>
2020-08-19 02:24:17 +0900

    god-seohee

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import UserInfo, Review, Comment, Item
 # Register your models here.
+admin.site.register(UserInfo)
+admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Item)
